ad5a74a653136e794184e645bc211f2c38c0184a
wellWINeo <stepan@uspenskiy.su>
2024-06-11 10:31:35 +0300

    use youtube cookies

--- handlers.py
+++ handlers.py
@@ -27,10 +27,15 @@ async def send_help(chat_id: int) -> None:
 
 
 async def send_youtube_video(chat_id: int, url: str) -> None:
-    save_path = os.environ["TEMP_VIDEO_PATH"]
+    tmp_path = os.environ["TEMP_VIDEO_PATH"]
+    cookies_path = os.path.join(tmp_path, "cookies.txt")
+
+    with open(cookies_path, 'w') as cookies_file:
+        cookies_file.writelines(os.environ["YT_COOKIES"])
 
     options = {
-        'outtmpl': os.path.join(save_path, "video"),
+        'outtmpl': os.path.join(tmp_path, "video"),
+        'cookies': cookies_path,
         'format': 'bestvideo[height<=1920]+bestaudio/best[height<=1080]/best',
         'ffmpeg_location': os.environ["FFMPEG_PATH"],
         'extractor_args': {'youtube:player_client': 'android'},
@@ -38,14 +43,17 @@ async def send_youtube_video(chat_id: int, url: str) -> None:
             'key': 'FFmpegVideoConvertor',
             'preferedformat': 'mp4'
         }],
+        'cachedir': False,
         'quiet': True,
     }
 
     with YoutubeDL(options) as ydl:
         metadata = ydl.extract_info(url, download=True)
 
-    video_file = os.listdir(save_path)[0]
-    video_path = os.path.join(save_path, video_file)
+    video_file = os.listdir(tmp_path)[0]
+    video_path = os.path.join(tmp_path, video_file)
+
+    print(f"File downloaded to {video_file}")
 
     async with get_bot() as bot:
         await bot.send_video(
